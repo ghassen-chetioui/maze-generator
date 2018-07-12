a9ae7a32bd313ccea164c340d8b4e2e74eb6b1c9
Ghassen Chetioui <g.chetioui-ext@lectra.com>
2018-07-12 18:03:40 +0200

    refacto: use constants for sizes

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,21 +1,24 @@
 // Magic for p5 importing properly in TypeScript AND Webpack
-import 'p5'; import Grid from './Grid';
+import 'p5';
+import Grid from './Grid';
 const p5 = require("p5");
 
 
 const sketch: p5 = new p5(() => { });
-const grid = Grid.build(10);
+
+const sketchSize = 400;
+const cellSize = 10;
+const grid = Grid.build(Math.floor(sketchSize / cellSize));
 
 sketch.setup = () => {
-  sketch.createCanvas(400, 400)
+  sketch.createCanvas(sketchSize, sketchSize)
 }
 
 sketch.draw = () => {
   sketch.background(50);
-  console.log(grid.cells().length)
   grid.cells().forEach(cell => {
     sketch.stroke(255);
     sketch.noFill();
-    sketch.rect(cell.position.x * 40, cell.position.y * 40, 40, 40);
+    sketch.rect(cell.position.x * cellSize, cell.position.y * cellSize, cellSize, cellSize);
   });
 }
